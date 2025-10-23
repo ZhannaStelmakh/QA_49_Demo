547aae7ad0c4385f2a625f8b75032eb7f364fc72
ZhannaStelmakh <vertebrology.center@gmail.com>
2025-10-23 17:50:12 +0300

    Type Practice Form

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,12 +1,22 @@
 package tests;
 
+import dto.Student;
 import manager.AppManager;
 import org.testng.annotations.Test;
+import pages.FormsPage;
+import pages.HomePage;
+import pages.PracticeFormPage;
 
 public class PracticeFormTests extends AppManager {
     @Test
     public void practiceFormPositiveTest(){
-
+        Student student = new Student("Petya","Petrov",
+                "petyapetrov@gmail.com", "Male",
+                "0123456789", "12 May 2002",
+                "Maths,Chemistry,Commerce", "Sports",""
+                ,"street 1", "NCR", "Delhi");
+        new HomePage((getDriver())).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPracticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
     }
-
 }
